6d5d351cef60e1b1fae26c7da86e746d36dcc344
Manuel Calvo <mmartinc@axpe.com>
2023-10-28 00:18:26 +0200

    add score listadapter and viewholder #5

---
+++ app/src/main/java/es/upm/miw/bantumi/views/ScoreListAdapter.java
@@ -0,0 +1,39 @@
+package es.upm.miw.bantumi.views;
+
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.DiffUtil;
+import androidx.recyclerview.widget.ListAdapter;
+
+import es.upm.miw.bantumi.model.Score;
+
+public class ScoreListAdapter extends ListAdapter<Score, ScoreViewHolder> {
+    public ScoreListAdapter(@NonNull DiffUtil.ItemCallback<Score> diffCallback) {
+        super(diffCallback);
+    }
+
+    @Override
+    public ScoreViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        return ScoreViewHolder.create(parent);
+    }
+
+    @Override
+    public void onBindViewHolder(ScoreViewHolder holder, int position) {
+        Score current = getItem(position);
+        holder.bind(current);
+    }
+
+    public static class ScoreDiff extends DiffUtil.ItemCallback<Score> {
+
+        @Override
+        public boolean areItemsTheSame(@NonNull Score oldItem, @NonNull Score newItem) {
+            return oldItem == newItem;
+        }
+
+        @Override
+        public boolean areContentsTheSame(@NonNull Score oldItem, @NonNull Score newItem) {
+            return oldItem.getDateTime().equals(newItem.getDateTime());
+        }
+    }
+}
